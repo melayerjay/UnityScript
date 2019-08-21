e36ed3351e93cdef7257ac574d8d69ce4a756049
melayerjay <melayer.jay@gmail.com>
2019-08-21 14:33:06 +0530

    trigger

---
+++ TriggerScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object Passed : " + other.gameObject.name);
+    }
+}
