302942ceebcc105372676785e8b7fa0abb8f8704
melayerjay <melayer.jay@gmail.com>
2019-08-23 17:34:55 +0530

    vectorAndQuate

---
+++ CubeRotation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    Quaternion rot;
+    Quaternion finalrot;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // rot = Quaternion.Euler(new Vector3(45,0,0));
+        // transform.rotation = rot;
+       // finalrot = Quaternion.Euler(new Vector3(0,180,0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation,finalrot,speed * Time.deltaTime);
+        //this.transform.rotation = Quaternion.Lerp(this.transform.rotation,Quaternion.identity,speed * Time.deltaTime);
+
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 temp = this.transform.localEulerAngles;
+        temp.y += MouseX * 2;
+        temp.x += MouseY *2;
+        //this.transform.localEulerAngles = temp;
+
+        //transform.Rotate(Vector3.up);
+        //transform.RotateAround(Vector3.zero,Vector3.right,speed * Time.deltaTime);
+
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        
+    }
+}
---
+++ cubeVector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeVector : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    Vector3 dirVector;
+    void Start()
+    {
+        dirVector = target.position - this.transform.position;
+        Debug.Log(dirVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //this.transform.position = Vector3.MoveTowards(this.transform.position, target.position,speed * Time.deltaTime);
+
+        //this.transform.position = Vector3.Lerp(this.transform.position, target.position,speed * Time.deltaTime);
+
+       // this.transform.position = Vector3.Slerp(this.transform.position, target.position, speed * Time.deltaTime);
+
+       // Debug.Log(Vector3.Distance(this.transform.position,target.position));
+
+      // Debug.Log(Vector3.Normalize(dirVector));
+    }
+}
