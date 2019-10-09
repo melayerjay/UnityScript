00d70c36242093c0e8b9d68386da34525c2269c5
melayerjay <melayer.jay@gmail.com>
2019-10-09 12:18:08 +0530

    UI

---
+++ CanvasControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CanvasControl : MonoBehaviour
+{
+    public Text UIText; 
+    public Slider UISlider;
+    public InputField UIInputField;
+    public GameObject UIPanel;
+    int count = 0;
+
+    public void MYButton(){
+        UIText.text = "HEllo";
+        UIPanel.SetActive(!UIPanel.activeSelf);
+    }  
+
+    public void MyToggle(bool state){
+        UIText.text = state.ToString();
+    }
+
+    public void MySlider(){
+        UIText.text = UISlider.value.ToString();
+    }
+
+    public void MyDropdown(int choice){
+        UIText.text = choice.ToString();
+    }
+
+    public void MyInputField(){
+        UIText.text = UIInputField.text;
+    }
+
+}
