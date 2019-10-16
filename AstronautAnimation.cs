7d4463130e60b11ea0947513f230ae6d0bb469ad
melayerjay <melayer.jay@gmail.com>
2019-10-16 12:18:34 +0530

    Animation

---
+++ AstronautAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstronautAnimation : MonoBehaviour
+{
+    bool walk;
+    Animator anim;
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        
+        if(move > 0 || move < 0){
+            walk = true;
+        }else{
+            walk = false;
+        }
+
+        anim.SetBool("walk",walk);
+    }
+}
---
+++ SoldierControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator anim;
+    public float speed;
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            //this.transform.position += Vector3.forward * speed * Time.deltaTime;
+            anim.SetBool("walk",true);
+        }
+        if(Input.GetKeyUp(KeyCode.W)){
+            anim.SetBool("walk",false);
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            //this.transform.position += Vector3.forward * speed * Time.deltaTime;
+            anim.SetBool("walkB",true);
+        }
+        if(Input.GetKeyUp(KeyCode.S)){
+            anim.SetBool("walkB",false);
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            anim.SetBool("kick",true);
+        }
+        if(Input.GetKeyUp(KeyCode.Space)){
+            anim.SetBool("kick",false);
+        }
+    }
+}
